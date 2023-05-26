4c16ad5f4229eb8b08d15e3aea79096d90af5387
melissinha <125404782+lopeslima@users.noreply.github.com>
2023-05-26 15:55:05 -0300

    Add files via upload

---
+++ main.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+  println("Olá mundo")
+  var nome = "Julia"
+  val nascimento = 2005 
+  
+  println(nome) // exibe o nome 
+  println(nascimento) // exibe o nascimento 
+
+  val myNum: Int = 5                // Int
+  val myDoubleNum: Double = 5.99    // Double
+  val myLetter: Char = 'D'          // Char
+  val myBoolean: Boolean = true     // Boolean
+  val myText: String = "Olá"      // String
+
+  println(myNum)
+  println(myDoubleNum)
+  println(myLatter)
+  println(myBoolean)
+  println(myText)
+}
