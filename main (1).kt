8b53f5fac5b654778aa05fc9081a8f1ee579228f
melissinha <125404782+lopeslima@users.noreply.github.com>
2023-05-29 16:21:46 -0300

    Add files via upload

---
+++ main (1).kt
@@ -0,0 +1,16 @@
+fun main() {
+print("Qual sua idade?")
+  var idade = readLine()!!
+  println ("Sua idade é: " + idade)
+  //numeros digitados no teclado sao interpretados como texto
+  //String
+  //convertendo string para Int
+  val num = idade.toInt()
+
+  if (num >= 18) {
+    println("Você é maior de idade")
+  }else{
+    println("Você é menor de idade")
+  }
+  
+}
