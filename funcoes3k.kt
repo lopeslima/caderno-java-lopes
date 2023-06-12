5060c6aa122427a3a830f9e525fee265b3a099b5
melissinha <125404782+lopeslima@users.noreply.github.com>
2023-06-12 15:12:13 -0300

    Create funcoes3k.kt

---
+++ funcoes3k.kt
@@ -0,0 +1,36 @@
+
+//criar uma função passando um parâmentro
+fun meuNome(nome:String){
+  println("Seu nome é: "+nome)
+}
+//crie uma função que retorna true ou false, passando parâmetro x na chamada da função 
+fun veroufalso(x:Boolean){
+  println(x)
+}
+//crie uma função que verifica a idade passada como paramentro, se é maior de idade
+seramaior divertido (y:Int){
+  se(y>=18){
+    println("É maior de idade: " + y)
+  }mais{
+    println("É menor de idade: " + y)
+  }
+}
+
+//crie uma função que passa dois parâmetros inteiros a e b, a função retorna a soma dos dois valores
+
+divertido soma(a: Int, b: Int):  Int {
+  retorno (a + b)
+}
+
+//função principal kotlin
+principal diversão () {
+ meuNome("Edney")
+ veroufalso(verdadeiro)
+ veroufalso(falso)
+
+ Seramaior(15)
+ Seramaior(19)
+
+  var resultado = soma(3, 5)
+  println(resultado)
+}
